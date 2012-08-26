0890109cf7a9d97a8d1f20119144163e42eafe93
Wydra <RileyCKR@gmail.com>
2012-08-26 11:20:05 -0400

    add AI to TCells

--- LD24/TCell.cs
+++ LD24/TCell.cs
@@ -9,6 +9,8 @@ namespace LD24
 {
     class TCell : Sprite
     {
+        int ticks = 0;
+
         public static TCell Build()
         {
             return new TCell(GameAssets.TCell1, new Rectangle(0, 0, 128, 128));
@@ -18,6 +20,20 @@ namespace LD24
             : base (texture, drawBounds)
         {
             this.Type = SpriteType.TCell;
+            this.Speed = 0.5F;
+        }
+
+        public override void Update(SceneGraph graph)
+        {
+            base.Update(graph);
+
+            ticks = ticks % 120;
+            if (ticks == 0)
+            {
+                 Velocity = RNG.RandomUnitVector();
+            }
+
+            ticks++;
         }
     }
 }
