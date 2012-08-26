b1b65bb14ef60c91604a2e39a834ac61433207e2
Wydra <RileyCKR@gmail.com>
2012-08-25 22:42:09 -0400

    Add assets for white blood cell

---
+++ LD24/TCell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LD24
+{
+    class TCell : Sprite
+    {
+        public TCell(Texture2D texture, Rectangle drawBounds)
+            : base (texture, drawBounds)
+        {
+            this.Type = SpriteType.TCell;
+        }
+    }
+}
