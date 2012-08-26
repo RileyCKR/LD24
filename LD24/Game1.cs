c7f64fe6eb59168de8962120fff6617d1136ad4d
Wydra <RileyCKR@gmail.com>
2012-08-25 22:57:04 -0400

    removed reference to WickedEngine

--- LD24/Game1.cs
+++ LD24/Game1.cs
@@ -9,8 +9,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
-using WickedEngine.Input;
-
 namespace LD24
 {
     /// <summary>
